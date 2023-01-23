45f301549705643509b58d20aa9e7b7de8d331bd
Lordracfures <raul.pineda.caballero@gmail.com>
2023-01-24 01:27:25 +0200

    listview builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,49 +4,48 @@ import 'package:intl/intl.dart';
 import '../models/transaccion.dart';
 import 'new_transaction.dart';
 
-
-
 class TransactionList extends StatelessWidget {
-   
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
- 
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: this.transactions.map(((tx) {
-            return Card(
-              color: Colors.green,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.red, width: 2)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      tx.amount.toString(),
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: Colors.green,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    transactions[index].toString(),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold),
-                      ),
-                      Text(DateFormat().format(tx.date)),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          })).toList()),
+                    Text(DateFormat().format(transactions[index].date)),
+                  ],
+                ),
+              ],
+            ),
+          );
+          ;
+        },
+        itemCount: transactions.length, //limite de elementos que queremos rederizar, sino sale error ya que hay elementos infinitos
+      ),
     );
   }
 }
